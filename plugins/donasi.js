73e15d90a9320bffa58f87dec093640022004a1e
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-15 16:43:25 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
 
     let img = fs.readFileSync('./thumbnail.jpg');
     let img2 = `https://telegra.ph/file/8f7e2b662ac8dc4f7f562.jpg`
-    return conn.sendButton(m.chat, 'Pm Owner aja \n❏ https://wa.me/628812743180 ', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
+    return conn.sendButton(m.chat, 'Pm Owner aja \n❏ https://wa.me/6281333154367 ', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
         contextInfo: {
             externalAdReply: {
                 mediaUrl: "https://instagram.com/saep.02",
