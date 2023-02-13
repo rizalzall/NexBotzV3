f476a7efd63581b0fc6266284c43d2ed6d77968e
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-02-13 09:03:34 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command, args }) => {
    if (!args[0]) return m.reply(`Masukan Url TikTok !`)
-   let res = await fetch(`https://api.ibeng.tech/api/download/tiktok?url=${args[0]}&apikey=ibeng`)
+   let res = await fetch(`https://saipulanuar.ga/api/download/tiktok?url=${args[0]}`)
   if (!res.ok) throw await res.text()
 let json = await res.json()
    let cap = `Cara Simpan\n\n1. Download Dulu Vidio Nya Kalau Sudah\n2. Masuk Ke Dalam Vidio Nya\n3. Lalu Tekan Titik3 Pojok Kanan Atas\n4. Lalu Pilih Yang Ada Bacaan  Simpan `
-conn.sendFile(m.chat, json.result.video, 'tiktok.mp4', cap, m)
+await conn.sendButtonVid(m.chat, json.result.video_original, cap, wm, 'ᴀᴜᴅɪᴏ', `.tiktokaudio ${args}`, m)
 }
 handler.help = ['ttnowm']
 handler.tags = ['downloader']
